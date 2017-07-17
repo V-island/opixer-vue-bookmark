ccdafa6fb139810fc55dddde4f8307c4d788d449
LW <vv15802702853@163.com>
2017-07-18 00:25:07 +0800

    更新less引入，模态框组件测试

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -45,77 +45,71 @@ export default {
 };
 </script>
 
-<style lang="less" scoped>
-//theme
-@theme-color: #37474f;
-@theme-font-color: #fff;
-//material
-@md-grey-400: #bdbdbd;
-@md-grey-500: #9e9e9e;
-@md-grey-600: #757575;
-.content{
-	// padding: 10px 0;
-}
-.action-bar{
-	-webkit-box-shadow: 0 1px 0 rgba(0,0,0,0.05);
-	box-shadow: 0 1px 0 rgba(0,0,0,0.05);
-	background: #fff;
-	height: 48px;
-	position: relative;
-	top: 0;
-	width: 100%;
-	.action-bar-item{
-		display: inline-block;
-		height: 48px;
-		vertical-align: top;
+<style lang="less">
+	@import '../assets/less/app';
+	.content{
+		// padding: 10px 0;
 	}
-	.action-bar-spacer{
-		background: #d6d6d6;
-	    display: inline-block;
-	    height: 64%;
-	    margin: 9px 5px;
-	    padding: 0;
-	    width: 1px;
-	}
-	.action-bar-link{
-		-webkit-font-smoothing: antialiased;
-		color: @md-grey-600;
-		cursor: pointer;
-		display: inline-block;
-		font-size: 14px;
-		font-weight: 400;
+	.action-bar{
+		-webkit-box-shadow: 0 1px 0 rgba(0,0,0,0.05);
+		box-shadow: 0 1px 0 rgba(0,0,0,0.05);
+		background: #fff;
 		height: 48px;
-		line-height: 48px;
-		text-decoration: none;
-		vertical-align: top;
-		&:hover{
-		    background-color: rgba(0,0,0,0.05);
+		position: relative;
+		top: 0;
+		width: 100%;
+		.action-bar-item{
+			display: inline-block;
+			height: 48px;
+			vertical-align: top;
 		}
-	}
-	.action-bar-link{
-		.anchor, .anchor:visited{
+		.action-bar-spacer{
+			background: #d6d6d6;
+		    display: inline-block;
+		    height: 64%;
+		    margin: 9px 5px;
+		    padding: 0;
+		    width: 1px;
+		}
+		.action-bar-link{
+			-webkit-font-smoothing: antialiased;
+			color: @md-grey-600;
+			cursor: pointer;
+			display: inline-block;
+			font-size: 14px;
+			font-weight: 400;
+			height: 48px;
+			line-height: 48px;
 			text-decoration: none;
-			-webkit-box-sizing: border-box;
-		    box-sizing: border-box;
-		    color: @md-grey-600;
-		    display: block;
-		    font-weight: 400;
-		    height: 100%;
-		    padding: 0 14px;
-		    text-align: left;
-		    width: 100%;
+			vertical-align: top;
+			&:hover{
+			    background-color: rgba(0,0,0,0.05);
+			}
 		}
-		&.active{
-			border-bottom-style: solid;
-			border-bottom-width: 3px;
-			color: @theme-color;
-			font-weight: 500;
-			height: 45px;
-			.anchor{
-				color: #333;
+		.action-bar-link{
+			.anchor, .anchor:visited{
+				text-decoration: none;
+				-webkit-box-sizing: border-box;
+			    box-sizing: border-box;
+			    color: @md-grey-600;
+			    display: block;
+			    font-weight: 400;
+			    height: 100%;
+			    padding: 0 14px;
+			    text-align: left;
+			    width: 100%;
+			}
+			&.active{
+				border-bottom-style: solid;
+				border-bottom-width: 3px;
+				color: @theme-color;
 				font-weight: 500;
+				height: 45px;
+				.anchor{
+					color: #333;
+					font-weight: 500;
+				}
 			}
 		}
 	}
-}
 </style>
